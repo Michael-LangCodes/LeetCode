6fd6df449acc0bfc241c89373540cf689dfa9d30
MichaelCodes <michael.lang3303@gmail.com>
2026-05-10 20:40:10 -0500

    May 10th

---
+++ 2553_Separate_Digits_In_Array/main.js
@@ -0,0 +1,21 @@
+// Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+// To separate the digits of an integer is to get all the digits it has in the same order.
+
+// For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+
+//My Solution
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+    let output = [];
+    nums.forEach((e,i)=>{
+        let mid = e.toString().split('');
+        mid.forEach((e,i)=>{
+            output.push(Number(e))
+        })
+    })
+    return output
+};
