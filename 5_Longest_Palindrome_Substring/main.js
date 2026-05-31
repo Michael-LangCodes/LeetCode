9191df8335e6d15fd48cbea3db249043b0b6cf75
MichaelCodes <michael.lang3303@gmail.com>
2026-05-30 23:14:00 -0500

    May 30th

---
+++ 5_Longest_Palindrome_Substring/main.js
@@ -0,0 +1,33 @@
+// Given a string s, return the longest palindromic substring in s.
+
+
+//Solution
+var longestPalindrome = function(s) {
+    let longest = "";
+
+    function expand(left, right) {
+        while (
+            left >= 0 &&
+            right < s.length &&
+            s[left] === s[right]
+        ) {
+            left--;
+            right++;
+        }
+
+        return s.slice(left + 1, right);
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let odd = expand(i, i);
+        let even = expand(i, i + 1);
+
+        let longer = odd.length > even.length ? odd : even;
+
+        if (longer.length > longest.length) {
+            longest = longer;
+        }
+    }
+
+    return longest;
+};
