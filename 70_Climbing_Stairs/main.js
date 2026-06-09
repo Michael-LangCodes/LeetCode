806e48f8959c1e6c6627ed2b08e5b7ad16e72673
MichaelCodes <michael.lang3303@gmail.com>
2026-06-08 20:01:32 -0500

    June 8th

--- 70_Climbing_Stairs/main.js
+++ 70_Climbing_Stairs/main.js
@@ -3,3 +3,14 @@
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 //Solution
+var climbStairs = function(n) {
+    if (n <= 2) return n;
+    let first = 1;
+    let second = 2;
+    for (let i = 3; i <= n; i++) {
+        let current = first + second;
+        first = second;
+        second = current;
+    }
+    return second;
+};
