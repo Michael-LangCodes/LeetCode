05f269411599ac9b573f85d7c1705b9a548b9a73
MichaelCodes <michael.lang3303@gmail.com>
2026-06-24 11:27:21 -0500

    June 24th

--- 17_Letter_Combos_of_Phone_Num/main.js
+++ 17_Letter_Combos_of_Phone_Num/main.js
@@ -3,3 +3,39 @@
 // A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
 //Solution
+var letterCombinations = function(digits) {
+    if (digits.length === 0) return [];
+
+    const myMap = new Map([
+        ['2', ['a', 'b', 'c']],
+        ['3', ['d', 'e', 'f']],
+        ['4', ['g', 'h', 'i']],
+        ['5', ['j', 'k', 'l']],
+        ['6', ['m', 'n', 'o']],
+        ['7', ['p', 'q', 'r', 's']],
+        ['8', ['t', 'u', 'v']],
+        ['9', ['w', 'x', 'y', 'z']]
+    ]);
+
+    const output = [];
+
+    function backtrack(index, current) {
+        // Base case: we've built a complete combination
+        if (index === digits.length) {
+            output.push(current);
+            return;
+        }
+
+        // Get letters for the current digit
+        const letters = myMap.get(digits[index]);
+
+        // Try each possible letter
+        for (let letter of letters) {
+            backtrack(index + 1, current + letter);
+        }
+    }
+
+    backtrack(0, '');
+
+    return output;
+};
